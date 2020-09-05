8f2c7ada00a376da4d8ae815dc0676f5d75b1cdb
strannic123 <manager350@mail.ru>
2020-09-05 18:40:04 +0300

    fixed bugs

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -12,7 +12,6 @@ const showMoreStyles = (trigger, style) => {
             card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs', 'styles-2');
             card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
         })
-        btn.style.display = 'none';
         btn.remove();
     })
 
